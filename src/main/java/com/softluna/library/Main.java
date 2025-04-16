18a5d0a1303a4c5a6b9c61110b826bf05b436bf4
luna <liuyuhan411011@163.com>
2025-04-16 11:51:47 +0800

    chore: initialize project with basic structure

---
+++ src/main/java/com/softluna/library/Main.java
@@ -0,0 +1,8 @@
+package com.softluna.library;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用简易图书管理系统！");
+        // 登录模块 & 菜单系统等即将来袭
+    }
+}
